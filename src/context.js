93f0a793bb13ce4a4a3a69514361c1504b95e309
Anthony Flores <anthony.flores2293@gmail.com>
2020-06-19 22:36:32 -0700

    Added pop up modal, modal functionality, and cart skeleton to context

--- src/context.js
+++ src/context.js
@@ -5,16 +5,88 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct,
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct,
+    cartSubtotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
+  };
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let products = [];
+    storeProducts.forEach((item) => {
+      const singleItem = { ...item };
+      products = [...products, singleItem];
+    });
+    this.setState(() => {
+      return { products };
+    });
   };
 
-  handleDetail = () => {
-    console.log("hello from detail");
+  getItem = (id) => {
+    const product = this.state.products.find((item) => item.id === id);
+    return product;
   };
 
-  addToCart = () => {
-    console.log("hello from add to cart");
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+
+  addToCart = (id) => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(
+      () => {
+        return { products: tempProducts, cart: [...this.state.cart, product] };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
+
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
+  };
+
+  increment = (id) => {
+    console.log("increment method");
+  };
+
+  decrement = (id) => {
+    console.log("decrement method");
+  };
+
+  removeItem = () => {
+    console.log("remove item");
+  };
+
+  clearCart = () => {
+    console.log("cart cleared");
   };
 
   render() {
@@ -24,6 +96,12 @@ class ProductProvider extends Component {
           ...this.state,
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
+          openModal: this.openModal,
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart,
         }}
       >
         {this.props.children}
